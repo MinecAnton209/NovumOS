805aba44637ad8b635f416efa7f982d187439648
MinecAnton209 <113017881+MinecAnton209@users.noreply.github.com>
2026-09-25 10:12:27 +0300

    chore(release): bump to 0.25-beta.2 with codename Tompouce
    - set NOVUMOS_VERSION and NOVA_VERSION to 0.25-beta.2
    - set NOVUMOS_CODENAME to Tompouce

--- zig/kernel/versioning.zig
+++ zig/kernel/versioning.zig
@@ -1,3 +1,3 @@
-pub const NOVUMOS_VERSION = "0.25-beta.1";
-pub const NOVUMOS_CODENAME = "Croiffin";
-pub const NOVA_VERSION = "0.25-beta.1";
+pub const NOVUMOS_VERSION = "0.25-beta.2";
+pub const NOVUMOS_CODENAME = "Tompouce";
+pub const NOVA_VERSION = "0.25-beta.2";
