201d17ac63d15477bc9e0442ef09a9853a229e43
MinecAnton209 <113017881+MinecAnton209@users.noreply.github.com>
2026-09-26 16:27:37 +0300

    chore(release): bump version to 0.25-beta.3 (Blintz)
    - Bump NOVUMOS_VERSION and NOVA_VERSION to 0.25-beta.3
    - Update NOVUMOS_CODENAME to Blintz

--- zig/kernel/versioning.zig
+++ zig/kernel/versioning.zig
@@ -1,3 +1,3 @@
-pub const NOVUMOS_VERSION = "0.25-beta.2";
-pub const NOVUMOS_CODENAME = "Tompouce";
-pub const NOVA_VERSION = "0.25-beta.2";
+pub const NOVUMOS_VERSION = "0.25-beta.3";
+pub const NOVUMOS_CODENAME = "Blintz";
+pub const NOVA_VERSION = "0.25-beta.3";
